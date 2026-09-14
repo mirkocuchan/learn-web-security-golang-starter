a6c5b3abb91d963d5850d8fd5af42b2c80670685
mirkocuchan <mirko.cuchan01@gmail.com>
2026-09-14 20:37:27 +0000

    new middleware

--- internal/httpserver/middleware.go
+++ internal/httpserver/middleware.go
@@ -28,6 +28,13 @@ func applyMiddleware(handler http.Handler, middlewareChain ...middleware) http.H
 	}
 	return handler
 }
+func contentTypeOptions(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
+		responseWriter.Header().Set("X-Content-Type-Options", "nosniff")
+
+		next.ServeHTTP(responseWriter, request)
+	})
+}
 
 func permissiveCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
