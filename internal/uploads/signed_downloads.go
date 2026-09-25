cd9a0d05324743e38c67037536b8e070b5478c62
mirkocuchan <mirko.cuchan01@gmail.com>
2026-09-25 14:47:02 +0000

    finishing authorization across reviews and the uploads

--- internal/uploads/signed_downloads.go
+++ internal/uploads/signed_downloads.go
@@ -1,6 +1,10 @@
 package uploads
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"crypto/subtle"
+	"encoding/hex"
 	"fmt"
 	"strconv"
 	"strings"
@@ -12,17 +16,41 @@ const signedDownloadTTL = 5 * time.Minute
 func CreateSignedDownloadPath(signingKey [32]byte, fileID int64, now time.Time) string {
 	expires := now.Unix() + int64(signedDownloadTTL.Seconds())
 	signature := signDownload(signingKey, fileID, expires)
-	return fmt.Sprintf("/files/%d/signed-download?expires=%d&signature=%s", fileID, expires, signature)
+
+	return fmt.Sprintf(
+		"/files/%d/signed-download?expires=%d&signature=%s",
+		fileID,
+		expires,
+		signature,
+	)
 }
 
-func VerifySignedDownload(_ [32]byte, _ int64, expiresValue, signature string, now time.Time) bool {
+func VerifySignedDownload(signingKey [32]byte, fileID int64, expiresValue, signature string, now time.Time) bool {
 	if expiresValue == "" || strings.Trim(expiresValue, "0123456789") != "" || len(signature) != 64 {
 		return false
 	}
 	expires, err := strconv.ParseInt(expiresValue, 10, 64)
-	return err == nil && expires > now.Unix()
+	if err != nil || expires <= now.Unix() {
+		return false
+	}
+	providedSignature, err := hex.DecodeString(signature)
+	if err != nil {
+		return false
+	}
+	expectedSignature, err := hex.DecodeString(signDownload(signingKey, fileID, expires))
+	if err != nil {
+		return false
+	}
+	return subtle.ConstantTimeCompare(providedSignature, expectedSignature) == 1
 }
 
-func signDownload(_ [32]byte, _, _ int64) string {
-	return strings.Repeat("0", 64)
+func signDownload(signingKey [32]byte, fileID, expires int64) string {
+	payload := fmt.Sprintf(
+		"GET\n/files/%d/signed-download\n%d",
+		fileID,
+		expires,
+	)
+	mac := hmac.New(sha256.New, signingKey[:])
+	mac.Write([]byte(payload))
+	return hex.EncodeToString(mac.Sum(nil))
 }
